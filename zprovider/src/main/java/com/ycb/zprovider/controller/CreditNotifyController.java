e69bd15e91554ad74977af3ab28a29c859e29702
duxinyuan <215041225@qq.com>
2017-09-27 10:02:08 +0800

    扫码租借controller

--- zprovider/src/main/java/com/ycb/zprovider/controller/CreditNotifyController.java
+++ zprovider/src/main/java/com/ycb/zprovider/controller/CreditNotifyController.java
@@ -58,21 +58,6 @@ public class CreditNotifyController {
     @RequestMapping(value = "/notify", method = {RequestMethod.POST})
     public void notify(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-        //获取支付宝POST过来反馈信息
-//        Map<String, String[]> requestMap = request.getParameterMap();
-//        Map<String, String> params = new HashMap<String, String>();
-//        for (Iterator iter = requestMap.keySet().iterator(); iter.hasNext(); ) {
-//            String name = (String) iter.next();
-//            String[] values = (String[]) requestMap.get(name);
-//            String valueStr = "";
-//            for (int i = 0; i < values.length; i++) {
-//                valueStr = (i == values.length - 1) ? valueStr + values[i]
-//                        : valueStr + values[i] + ",";
-//            }
-//            //乱码解决，这段代码在出现乱码时使用。如果mysign和sign不相等也可以使用这段代码转化
-//            //valueStr = new String(valueStr.getBytes("ISO-8859-1"), "gbk");
-//            params.put(name, valueStr);
-//        }
         Map<String, String> params = RequestUtil.getRequestParams(request);
 
         //1、签名验证
