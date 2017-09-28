07d54ac7e1e86c89160b1e3f11fa886252d8f811
duxinyuan <215041225@qq.com>
2017-09-28 11:08:58 +0800

    .

--- zprovider/src/main/java/com/ycb/zprovider/controller/CreditCompleteOrderController.java
+++ zprovider/src/main/java/com/ycb/zprovider/controller/CreditCompleteOrderController.java
@@ -36,25 +36,6 @@ public class CreditCompleteOrderController {
     @Autowired
     private AlipayClientFactory alipayClientFactory;
 
-    //初始化alipayClient用到的参数:该appId必须设为开发者自己的生活号id
-    @Value("${APPID}")
-    private String appId;
-    //初始化alipayClient用到的参数:该私钥为测试账号私钥  开发者必须设置自己的私钥,否则会存在安全隐患
-    @Value("${PRIVATE_KEY}")
-    private String privateKey;
-    //初始化alipayClient用到的参数:仅支持JSON
-    @Value("${FORMAT}")
-    private String format;
-    //初始化alipayClient用到的参数:字符编码-传递给支付宝的数据编码
-    @Value("${CHARSET}")
-    private String charset;
-    //初始化alipayClient用到的参数:该公钥为测试账号公钥,开发者必须设置自己的公钥 ,否则会存在安全隐患
-    @Value("${ALIPAY_PUBLIC_KEY}")
-    private String alipayPublicKey;
-    //初始化alipayClient用到的参数:签名类型
-    @Value("${SIGN_TYPE}")
-    private String signType;
-
     @Autowired
     private OrderMapper orderMapper;
 
