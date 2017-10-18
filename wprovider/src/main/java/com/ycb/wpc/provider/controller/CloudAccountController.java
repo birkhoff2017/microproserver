c873a7cb10a8fa569ae780888cc616ea8bc20b1c
Bruce <wrl19930220@163.com>
2017-10-18 15:52:39 +0800

    修改存入Redis的AccessToken的key

--- wprovider/src/main/java/com/ycb/wpc/provider/controller/CloudAccountController.java
+++ wprovider/src/main/java/com/ycb/wpc/provider/controller/CloudAccountController.java
@@ -49,14 +49,11 @@ public class CloudAccountController {
         Map<String, Object> bacMap = new HashMap<>();
 
         try {
-            String openid = this.redisService.getKeyValue(MD5.getMessageDigest(code.getBytes()));
-            if (StringUtils.isBlank(openid)) {
-                String param = "appid=" + appID + "&secret=" + appSecret + "&code=" + code + "&grant_type=authorization_code ";
-                String requestStr = HttpRequest.sendGet(GlobalConfig.WX_AUTH2_URL, param);
-                Map<String, Object> requestMap = JsonUtils.readValue(requestStr);
-                openid = (String) requestMap.get("openid");
-                this.redisService.setKeyValueTimeout(MD5.getMessageDigest(code.getBytes()), openid, Long.valueOf(expiresIn));
-            }
+            String param = "appid=" + appID + "&secret=" + appSecret + "&code=" + code + "&grant_type=authorization_code ";
+            String requestStr = HttpRequest.sendGet(GlobalConfig.WX_AUTH2_URL, param);
+            Map<String, Object> requestMap = JsonUtils.readValue(requestStr);
+            String openid = (String) requestMap.get("openid");
+            this.redisService.setKeyValueTimeout(MD5.getMessageDigest(code.getBytes()), openid, Long.valueOf(expiresIn));
             UserInfoVo userInfoVo = this.userMapper.findUserinfo(openid);
             if (null != userInfoVo) {
                 Map<String, Object> data = new HashMap<>();
